6b67f6f351786b6388eed935a94ef12fe65e48cd
park-damien <damien.park.personal@gmail.com>
2025-03-14 17:19:46 +0900

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/color-recommender/', // GitHub 저장소 이름으로 변경해주세요
+  base: '/personal-color/',
 }) 
